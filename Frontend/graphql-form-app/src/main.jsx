97f36f7edd47678e07fa8a00d9d49851056e36d1
bhuvnesh-gupta-deqo <bgupta@deqode.com>
2024-12-17 17:49:24 +0530

    Update main.jsx

--- Frontend/graphql-form-app/src/main.jsx
+++ Frontend/graphql-form-app/src/main.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import App from "./App";
-{
-  /* The following line can be included in your src/index.js or App.js file */
-}
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 const client = new ApolloClient({
-  uri: "http://localhost:5000", // Mock server URL
+  uri: "http://localhost:5000", 
   cache: new InMemoryCache(),
 });
 
